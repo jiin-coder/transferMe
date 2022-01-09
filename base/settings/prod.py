aaf015e34aee4674c8391926b2c105789f68e00f
jiin-coder <fpahstiq123@naver.com>
2022-01-10 01:55:10 +0900

    세팅

---
+++ base/settings/prod.py
@@ -0,0 +1,20 @@
+from .common import *
+
+DEBUG = False
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'site_p', # DB명
+        'USER': 'sbsstlocal', # DBMS 접속 아이디
+        'PASSWORD': 'sbs123414', # DBMS 접속 비번
+        'HOST': '172.17.0.1', # DBMS 주소
+        'PORT': '3306', # DBMS 포트
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
+    }
+}
+
