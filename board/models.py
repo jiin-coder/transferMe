79ede3cec8b0976d2b56eec6f1e18b98f6f689f3
jiin-coder <fpahstiq123@naver.com>
2022-01-20 10:33:30 +0900

    글 작성화면 디자인 수정

--- board/models.py
+++ board/models.py
@@ -9,8 +9,8 @@ class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="작성자")
     title = models.CharField(max_length=200, verbose_name="제목")
     body = models.TextField(verbose_name="내용")
-    one_source = models.TextField(verbose_name="출처명")
-    information_source = models.TextField(verbose_name="출처링크")
+    one_source = models.CharField(max_length=200, verbose_name="출처명")
+    information_source = models.CharField(max_length=200, verbose_name="출처링크")
     reg_date = models.DateTimeField('등록날짜', auto_now_add=True)
     update_date = models.DateTimeField('갱신날짜', auto_now=True)
 
