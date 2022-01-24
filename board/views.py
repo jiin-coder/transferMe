7c6cb934c8b16d089d93d2f97d71f61d6b7a7645
jiin-coder <fpahstiq123@naver.com>
2022-01-24 09:34:45 +0900

    권한있는 유저만 글 삭제 수정 가능하도록 구현

--- board/views.py
+++ board/views.py
@@ -13,12 +13,14 @@ from django.contrib import messages
 
 
 @login_required(login_url='accounts:signin')
-def article_delete(request, article_id):
+def article_delete(request: HttpRequest, article_id):
     article = get_object_or_404(Article, id=article_id)
+
     if request.user != article.writer:
         messages.error(request, '삭제권한이 없습니다')
         return redirect('board:detail', article_id=article.id)
     article.delete()
+    messages.success(request, "글이 삭제되었습니다.")
     return redirect('board:list')
 
 
