6fcd78a2a3a4beaa7d6476027e78c0bee980b21a
jiin-coder <fpahstiq123@naver.com>
2022-01-25 13:16:56 +0900

    카카오 로그인, 회원가입 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
     path('signout/', views.signout, name='signout'),
+    path('find_username/', views.find_username, name='find_username'),
+    path('signin/kakao/', views.kakao_login, name="kakao_signin"),
+    path('signin/kakao/callback/', views.kakao_login_callback, name="kakao_signin_callback"),
 ]
